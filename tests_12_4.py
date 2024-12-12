d32fef4600de1837f2a3e20b172d36afd629ebfa
vxndtt <alenakad16@mail.ru>
2024-12-12 16:52:16 +0500

    Add files via upload

---
+++ tests_12_4.py
@@ -0,0 +1,51 @@
+import logging
+import unittest
+import runner_and_tournament
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, filemode='w', filename='runner_tests.log', encoding='UTF-8',
+                        format='%(asctime)s | %(levelname)s | %(message)s')
+
+class RunnerTest(unittest.TestCase):
+
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_walk(self):
+        try:
+            runner = runner_and_tournament.Runner('Sasha', -5)
+            for i in range(10):
+                runner.walk()
+            self.assertEqual(runner.distance, 50)
+            logging.info('test_walk выполнен успешно')
+        except:
+            logging.warning('Неверная скорость для Runner', exc_info=True)
+
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_run(self):
+        try:
+            runner = runner_and_tournament.Runner(5)
+            for i in range(10):
+                runner.run()
+            self.assertEqual(runner.distance, 100)
+            logging.info('Неверный тип данных для объекта Runner')
+        except:
+            logging.warning('Неверный тип данных для объекта Runner', exc_info=True)
+
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_challenge(self):
+        runner1 = runner_and_tournament.Runner('Katya')
+        runner2 = runner_and_tournament.Runner('Igor')
+        for i in range(10):
+            runner1.walk()
+            runner2.run()
+        self.assertNotEqual(runner1.distance, runner2.distance)
+
+if __name__ == '__main__':
+    unittest.main()
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, filemode='w', filename='runner_tests.log', encoding='UTF-8',
+                        format='%(asctime)s | %(levelname)s | %(message)s')
